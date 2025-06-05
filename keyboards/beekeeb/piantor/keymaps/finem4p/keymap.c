a66ba0ccbbcb275c2288dd11c405879560e433f6
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-06-05 18:30:01 +0200

    feat: home row mods both sides

--- keyboards/beekeeb/piantor/keymaps/finem4p/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/finem4p/keymap.c
@@ -12,6 +12,16 @@ enum layer {
     _GAME,
     _GAME_NUM,
     _FG,
+    // Mozna sprobowac zrobic tak, zeby na jednej rece po przytrzymaniu danego hmr dostepne byly tylko kolejne modifiery
+    // w sensie chodzi o to ze jak przytrzymam gui to na lewej polowie nie dziala zadna litera, a dzialaja tylko kolejne modifiery
+    // _LSFT,
+    // _LALT,
+    // _LCTL,
+    // _LGUI,
+    // _RSFT,
+    // _RALT,
+    // _RCTL,
+    // _RGUI,
 };
 
 // See:
@@ -43,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*),*/
 
     [_BASE] = LAYOUT_split_3x6_3(
-        KC_ESC,                 KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                       KC_Y,   KC_U,   KC_I,       KC_O,    KC_P,      KC_BSLS,
-        KC_TAB,                 LSFT_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LGUI_T(KC_F),   KC_G,                       KC_H,   KC_J,   KC_K,       KC_L,    KC_SCLN,   KC_QUOT,
-        KC_LSFT,                KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                       KC_N,   KC_M,   KC_COMM,    KC_DOT,  KC_SLSH,   KC_RSFT,
+        KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                       KC_Y,   KC_U,   KC_I,       KC_O,    KC_P,      KC_BSLS,
+        KC_TAB, LSFT_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LGUI_T(KC_F),   KC_G,   KC_H,   RGUI_T(KC_J),   RCTL_T(KC_K),   LALT_T(KC_L), RSFT_T(KC_SCLN),   KC_QUOT,
+        KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                       KC_N,   KC_M,   KC_COMM,    KC_DOT,  KC_SLSH,   KC_RSFT,
                                    KC_LGUI, LT(_MEDIA, KC_SPC), KC_LCTL,                RALT_T(KC_BSPC),  LT(_VIM, KC_ENT),   MO(_NUM)
     ),
 
