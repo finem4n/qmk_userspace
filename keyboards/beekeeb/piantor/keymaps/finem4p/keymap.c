6b98b1908fcd0a0e021fcd95e2fd9baa3d0cfdbe
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-06-05 20:38:03 +0200

    feat(num): add parenthesis on a and s

--- keyboards/beekeeb/piantor/keymaps/finem4p/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/finem4p/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*),*/
 
     [_BASE] = LAYOUT_split_3x6_3(
-        KC_ESC, MT(MOD_1, KC_Q),   KC_W,   KC_E,   KC_R,   KC_T,                       KC_Y,   KC_U,   KC_I,       KC_O,    KC_P,      KC_BSLS,
+        KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                       KC_Y,   KC_U,   KC_I,       KC_O,    KC_P,      KC_BSLS,
         KC_TAB, LSFT_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LGUI_T(KC_F),   KC_G,   KC_H,   RGUI_T(KC_J),   RCTL_T(KC_K),   LALT_T(KC_L), RSFT_T(KC_SCLN),   KC_QUOT,
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                       KC_N,   KC_M,   KC_COMM,    KC_DOT,  KC_SLSH,   KC_RSFT,
                                    KC_LGUI, LT(_MEDIA, KC_SPC), KC_LCTL,                RALT_T(KC_BSPC),  LT(_VIM, KC_ENT),   MO(_NUM)
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUM] = LAYOUT_split_3x6_3(
         KC_GRV,     KC_1,   KC_2,   KC_3,       KC_4,       KC_5,                           KC_6,   KC_7,       KC_8,       KC_9,   KC_0,   KC_NO,
-        KC_LALT,    KC_NO,  KC_NO,  KC_MINS,    KC_EQL,     KC_NO,                          KC_NO,  KC_LBRC,    KC_RBRC,    KC_NO,  KC_F10, KC_F11,
+        KC_LALT,    S(KC_9),  S(KC_0),  KC_MINS,    KC_EQL,     KC_NO,                          KC_NO,  KC_LBRC,    KC_RBRC,    KC_NO,  KC_F10, KC_F11,
         KC_LSFT,    KC_F1,  KC_F2,  KC_F3,      KC_F4,      KC_F5,                          KC_F6,  KC_F7,      KC_F8,      KC_F9,  KC_F12, KC_RSFT,
                                         KC_LGUI,    CW_TOGG,    KC_LCTL,        KC_NO,  KC_NO,   KC_TRNS
     ),
