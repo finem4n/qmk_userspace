769ecdaaad2b698188e4f9242a3ae5af9d5b4f16
Konrad Marciniak <marciniakkonrad@protonmail.com>
2025-06-10 21:49:46 +0200

    feat: callum test

--- keyboards/beekeeb/piantor/keymaps/finem4p_callum/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/finem4p_callum/keymap.c
@@ -61,6 +61,23 @@ enum custom_keycodes {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // static uint16_t media_timer;
 
+    update_oneshot(
+        &os_shft_state, KC_LSFT, OS_SHFT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_ctrl_state, KC_LCTL, OS_CTRL,
+        keycode, record
+    );
+    update_oneshot(
+        &os_alt_state, KC_LALT, OS_ALT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_cmd_state, KC_LCMD, OS_GUI,
+        keycode, record
+    );
+
     switch (keycode) {
         case REDO:
             if (record->event.pressed) {
@@ -244,26 +261,6 @@ oneshot_state os_ctrl_state = os_up_unqueued;
 oneshot_state os_alt_state = os_up_unqueued;
 oneshot_state os_cmd_state = os_up_unqueued;
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    update_oneshot(
-        &os_shft_state, KC_LSFT, OS_SHFT,
-        keycode, record
-    );
-    update_oneshot(
-        &os_ctrl_state, KC_LCTL, OS_CTRL,
-        keycode, record
-    );
-    update_oneshot(
-        &os_alt_state, KC_LALT, OS_ALT,
-        keycode, record
-    );
-    update_oneshot(
-        &os_cmd_state, KC_LCMD, OS_GUI,
-        keycode, record
-    );
-
-    return true;
-}
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, SYM, NAV, NUM);
